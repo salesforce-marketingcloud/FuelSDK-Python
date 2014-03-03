89109ae9d3fe686d93b022f8f45334f7d0870a1b
Charlie Moad <cmoad@exacttarget.com>
2014-03-02 21:03:21 -0500

    added deprecation warning to old module

--- ET_Client.py
+++ ET_Client.py
@@ -1,3 +1,6 @@
+import warnings
+warnings.warn('Import the FuelSDK module instead. This module will be removed in the future.', DeprecationWarning)
+
 # For legacy support only.
 # Use FuelSDK module instead (Example: from FuelSDK import ET_Client)
 from FuelSDK import *
