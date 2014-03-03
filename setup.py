0a7032dd70351f9ce49e75af4ddd55e0b428350a
Charlie Moad <cmoad@exacttarget.com>
2014-03-02 21:09:29 -0500

    updated version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md') as f:
     readme = f.read()
 
 setup(
-    version='0.9.0',
+    version='0.9.1',
     name='FuelSDK',
     description='ExactTarget Fuel SDK for Python',
     long_description=readme,
