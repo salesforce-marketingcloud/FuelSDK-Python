7e00e90d953bfadae55ecc3b88b14bc80e4f7503
Ismaïl MOURTADA <ismail.mourtada@gmail.com>
2020-02-24 23:58:34 +0100

    Base exception class for all custom exceptions

--- FuelSDK/exceptions.py
+++ FuelSDK/exceptions.py
@@ -1,14 +1,19 @@
-class ConfigurationException(Exception):
+class ET_Client_Exception(Exception):
+    """
+    Base class for all exception happening at the ET_Client level
+    """
+
+class ConfigurationException(ET_Client_Exception):
     """
     Raised when an error related to the Client configuration occurs
     """
 
-class AuthException(Exception):
+class AuthException(ET_Client_Exception):
     """
     Raised when the authentication to the Client fails
     """
 
-class WSDLException(Exception):
+class WSDLException(ET_Client_Exception):
     """
     Raised when getting the WSDL file fails
     """
