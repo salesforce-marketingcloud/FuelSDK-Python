c812a3468657f791f0ea13799650c396df8f20ee
Miguel G. Flores <miguel.gonzalez@feverup.com>
2020-12-16 17:18:39 +0100

    Move try except to soap call

--- FuelSDK/client.py
+++ FuelSDK/client.py
@@ -361,10 +361,14 @@ class ET_Client(object):
 
     def get_soap_cache_file(self):
         json_data = {}
-        if os.path.isfile(self.soap_cache_file):
-            file = open(self.soap_cache_file, "r")
-            json_data = json.load(file)
-            file.close()
+        
+        try:
+	        if os.path.isfile(self.soap_cache_file):
+	            file = open(self.soap_cache_file, "r")
+	            json_data = json.load(file)
+	            file.close()
+	    except Exception:
+	    	return json_data
 
         return json_data
 
@@ -380,17 +384,16 @@ class ET_Client(object):
     def get_soap_endpoint(self):
         default_endpoint = 'https://webservice.exacttarget.com/Service.asmx'
 
+        cache_file_data = self.get_soap_cache_file()
+
+        if 'url' in cache_file_data and 'timestamp' in cache_file_data \
+            and cache_file_data['timestamp'] > time.time():
+            return cache_file_data['url']
+
+        """
+        find the correct url that data request web calls should go against for the token we have.
+        """
         try:
-        	cache_file_data = self.get_soap_cache_file()
-
-	        if 'url' in cache_file_data and 'timestamp' in cache_file_data \
-	            and cache_file_data['timestamp'] > time.time():
-	            return cache_file_data['url']
-
-	        """
-	        find the correct url that data request web calls should go against for the token we have.
-	        """
-        
             r = requests.get(self.base_api_url + '/platform/v1/endpoints/soap', headers={
                 'user-agent': 'FuelSDK-Python-v1.3.0',
                 'authorization': 'Bearer ' + self.authToken
