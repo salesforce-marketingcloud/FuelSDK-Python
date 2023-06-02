a809546a1cea2c19979a85ac38ede23f24785109
Tsepo Ramaisa <tsepo.ramaisa@prodigyfinance.com>
2023-06-02 11:29:39 +0200

    remove ET_Client

--- ET_Client.py
+++
@@ -1,6 +0,0 @@
-import warnings
-warnings.warn('Import the FuelSDK module instead. This module will be removed in the future.', DeprecationWarning)
-
-# For legacy support only.
-# Use FuelSDK module instead (Example: from FuelSDK import ET_Client)
-from FuelSDK import *
